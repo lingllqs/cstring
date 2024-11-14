0863d93e882cea9ff3e6f3cd06a5358c81a20740
lingllqs <lingllqs@163.com>
2024-11-14 22:04:19 +0800

    源码

--- src/cstring.c
+++ src/cstring.c
@@ -1,10 +1,12 @@
+#define _GNU_SOURCE
+#include "include/cstring.h"
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "include/utils.h"
-#include "include/cstring.h"
 
 String *new_string(const char *str) {
     String *string = malloc(sizeof(String));
@@ -13,58 +15,57 @@ String *new_string(const char *str) {
     }
 
     uint32_t str_len = strlen(str);
-    string->str      = malloc((str_len + 1));
+    string->str = malloc((str_len + 1));
     if (NULL == string->str) {
         free(string);
         perror("malloc str");
         return NULL;
     }
 
-    strcpy(string->str, str);
+    string->str = strdup(str);
 
     StrAttr str_attr = get_str_attr(str);
-    string->length   = str_len;
-    string->nlength  = str_attr.cell_len;
-    string->zh_count = str_attr.zh_nums;
+    string->str_attr.length = str_attr.length;
+    string->str_attr.nlength = str_attr.nlength;
+    string->str_attr.zh_count = str_attr.zh_count;
 
     return string;
 }
 
 StrAttr get_str_attr(const char *str) {
-    uint32_t zh_nums    = 0;  // 中文或者其他非 ascii 字符个数
-    uint32_t char_len   = 0;  // 字符所占的字节数
-    uint32_t str_length = 0;
-    const char *ptr     = str;
+    uint32_t zh_count = 0;  // 中文或者其他非 ascii 字符个数
+    uint32_t char_len = 0;  // 字符所占的字节数
+    uint32_t length = 0;
+    uint32_t nlength = 0;
+    const char *ptr = str;
 
     while (*ptr) {
         /* ascii 字符 */
         if ((*ptr & 0x80) == 0x00) {
-            char_len    = 1;
-            str_length += 1;
+            char_len = 1;
         }
         /* 2 个字节表示一个字符 */
         else if ((*ptr & 0xe0) == 0xc0) {
-            char_len    = 2;
-            str_length += 1;
-            zh_nums    += 1;
+            char_len = 2;
+            zh_count += 1;
         }
         /* 3 个字节表示一个字符 */
         else if ((*ptr & 0xf0) == 0xe0) {
-            char_len    = 3;
-            str_length += 1;
-            zh_nums    += 1;
+            char_len = 3;
+            zh_count += 1;
         }
         /* 4 个字节表示一个字符 */
         else if ((*ptr & 0xf8) == 0xf0) {
-            char_len    = 4;
-            str_length += 1;
-            zh_nums    += 1;
+            char_len = 4;
+            zh_count += 1;
         }
 
+        length += char_len;
+        nlength += 1;
         ptr += char_len;
     }
 
-    StrAttr strattr = {.cell_len = str_length, .zh_nums = zh_nums};
+    StrAttr strattr = {.nlength = nlength, .zh_count = zh_count, .length = length};
     return strattr;
 }
 
@@ -72,7 +73,7 @@ void print_string(String string) {
     printf(
         "String {\n    str = \"%s\"\n    zh_count = %d\n    length = %u\n    "
         "nlength = %d\n}\n",
-        string.str, string.zh_count, string.length, string.nlength
+        string.str, string.str_attr.zh_count, string.str_attr.length, string.str_attr.nlength
     );
 }
 
--- src/include/cstring.h
+++ src/include/cstring.h
@@ -3,21 +3,20 @@
 
 #include <stdint.h>
 
-typedef struct String {
-    char    *str;       // 字符串
-    uint32_t zh_count;  // 中文个数
-    uint32_t length;    // 字符串长度
-    uint32_t nlength;   // 中文算一个 char
-} String;
-
 typedef struct StrAttr {
-    uint32_t cell_len;  // 终端中占用多少个格子
-    uint32_t zh_nums;   // 中文个数
+    uint32_t nlength;   // 终端中占用多少个格子
+    uint32_t length;    // 字符串长度(中文算实际字节数)
+    uint32_t zh_count;  // 中文个数
 } StrAttr;
+
+typedef struct String {
+    char *str;  // 字符串
+    StrAttr str_attr;//属性
+} String;
 
 String *new_string(const char *str);    // 新建 String
 StrAttr get_str_attr(const char *str);  // 获取字符串属性
-void    print_string(String string);    // 打印 String
-void    free_string(String *string);    // 释放资源
+void print_string(String string);       // 打印 String
+void free_string(String *string);       // 释放资源
 
 #endif
